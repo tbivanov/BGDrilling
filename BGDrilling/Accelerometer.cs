e5eca9bf0e031a097a0cf8452767360b788d2b14
Tihomir Ivanov <tbivanov@fmi.uni-sofia.bg>
2016-09-19 17:49:27 +0300

    Sensor classes 1

---
+++ BGDrilling/Accelerometer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGDrilling
+{
+    class Accelerometer : Sensor
+    {
+        public override void calibrate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal incl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal tf()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BGDrilling/Sensor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGDrilling
+{
+    abstract class Sensor
+    {
+        private decimal[,] M = new decimal[3, 3], b = new decimal [3,1];
+        public Sensor()
+        {
+
+        }
+        public abstract void calibrate();
+        public abstract decimal tf();
+        public abstract decimal incl();
+    }
+}
