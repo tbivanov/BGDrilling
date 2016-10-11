a27b86805b9ca4378a18ddb079f49a8629a07698
Galina Lyutskanova <g.lyutskanova@gmail.com>
2016-10-11 15:49:46 +0300

    Cholesky...

--- BGDrilling/BGDrilling/LinearAlgebra.cs
+++ BGDrilling/BGDrilling/LinearAlgebra.cs
@@ -16,5 +16,35 @@ namespace BGDrilling
             }
 
         }
+
+        public static decimal[,] Cholesky (decimal[,] A)
+        {
+            int rows = A.GetLength(0);
+            int cols = A.GetLength(1);
+            decimal[,] L = new decimal[rows, cols];/*lower triangular*/
+
+            /*if (rows==cols & MathDecimal.Transpose(B)=B)*/
+            for (int k = 0; k < cols; k++)
+            {
+                decimal sum = 0;
+
+                for (int p=0; p<=k-2; p++)
+                    sum += L[k, p] * L[k, p];
+
+                L[k, k] = MathDecimal.Sqrt(A[k, k] - sum);
+
+                for( int j=k; j<cols; j++)
+                {
+                    sum = 0;
+
+                    for (int p = 0; p <= k - 2; p++)
+                        sum += L[k, p] * L[j, p];
+
+                    A[j,k] = (A[k, j] - sum) / L[k, k];
+                }
+            }
+           
+            return L;
+        }
     }
 }
