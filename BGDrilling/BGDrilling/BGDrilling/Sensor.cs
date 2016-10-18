4198916c5dfc654265b8dbf2d0a6339b7106a089
Galina Lyutskanova <g.lyutskanova@gmail.com>
2016-10-18 12:23:13 +0300

    Backward

---
+++ BGDrilling/BGDrilling/BGDrilling/Sensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGDrilling
+{
+    abstract class Sensor
+    {
+        public List<Measurement> data { get; set; }
+        public CalibrationParameter pars { get; set; }
+        public Sensor():this(new List<Measurement>())
+        {
+          
+        }
+        public Sensor(List<Measurement> data, CalibrationParameter pars = null)
+        {
+            this.data = data;
+            this.pars = pars;
+        }
+        public abstract decimal[] calibrate();
+        public virtual void compute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
